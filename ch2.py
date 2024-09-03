41eaeb43243a91043ced1017fdb3547540ed3216
bean-frog <gkieran32855@gmail.com>
2024-09-03 13:43:55 -0700

    ch2 questions partner activity

---
+++ ch2.py
@@ -0,0 +1,15 @@
+# 2
+#name = input("who tf are you\n")
+#print("hello " + name)
+
+#3
+#hours = int(input("how many hours did you work? \n >"))
+#rate = float(input("what is your hourly pay? \n >"))
+#print (hours * rate)
+
+#4
+# (a) 8
+# (b) 8.0
+# (c) 4.0
+# (d) syntax error
+
